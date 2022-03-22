c71ca28d5f9d51e6121f792e9daf609cdaede003
Sanjiv Kawa <sanjivkawa@gmail.com>
2022-03-22 09:46:52 -0400

    + v1.2

--- SQLRecon/SQLRecon/authentication/Windows.cs
+++ SQLRecon/SQLRecon/authentication/Windows.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.Data.SqlClient;
-using SQLRecon.Auth;
 
 namespace SQLRecon.Auth
 {
@@ -10,7 +8,6 @@ namespace SQLRecon.Auth
         // this handles Windows authentication to MS SQL databases
         public SqlConnection Send(String sqlServer, String database)
         {
-            //Console.WriteLine("[+] Windows Authentication Selected");
             string user = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
             String conString = "Server = " + sqlServer + "; Database = " + database + "; Integrated Security = True;";
             TestAuthentication TestAuthentication = new TestAuthentication();
