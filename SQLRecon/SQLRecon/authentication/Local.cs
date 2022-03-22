2a438d99cee85b95b7edfce996bff0d79a1b2e28
Sanjiv Kawa <sanjivkawa@gmail.com>
2022-03-22 09:46:19 -0400

    + v1.2

--- SQLRecon/SQLRecon/authentication/Local.cs
+++ SQLRecon/SQLRecon/authentication/Local.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.Data.SqlClient;
-using SQLRecon.Auth;
 
 namespace SQLRecon.Auth
 {
@@ -10,7 +8,6 @@ namespace SQLRecon.Auth
         // this handles local authentication to MS SQL databases
         public SqlConnection Send(String sqlServer, String database, String user, String pass)
         {
-            //Console.WriteLine("[+] Local Authentication Selected");
             String conString = "Server = " + sqlServer + "; Database = " + database + "; Integrated Security=false; user id=" + user + "; password=" + pass + ";";
 
             TestAuthentication TestAuthentication = new TestAuthentication();
