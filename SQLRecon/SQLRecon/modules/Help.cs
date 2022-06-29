3169daafdd7b88b8f73ea90b5b5b6f29712e4f6b
Sanjiv Kawa <sanjivkawa@gmail.com>
2022-06-29 16:41:54 -0400

    v2.1.4

--- SQLRecon/SQLRecon/modules/Help.cs
+++ SQLRecon/SQLRecon/modules/Help.cs
@@ -13,7 +13,7 @@ namespace SQLRecon.Modules
         public void initialize()
         {
             Console.WriteLine("");
-            Console.WriteLine("SQLRecon v2.1.3");
+            Console.WriteLine("SQLRecon v2.1.4");
             Console.WriteLine("github.com/skahwah/SQLRecon");
             Console.WriteLine("");
 
