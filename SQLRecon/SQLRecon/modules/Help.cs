213fc18b69859cd8913648124b1618b282d73de4
Rasta Mouse <rasta@rastamouse.me>
2023-01-13 11:14:02 +0000

    Increment minor version number

--- SQLRecon/SQLRecon/modules/Help.cs
+++ SQLRecon/SQLRecon/modules/Help.cs
@@ -13,7 +13,7 @@ namespace SQLRecon.Modules
         public void initialize()
         {
             Console.WriteLine("");
-            Console.WriteLine("SQLRecon v2.1.4");
+            Console.WriteLine("SQLRecon v2.1.5");
             Console.WriteLine("github.com/skahwah/SQLRecon");
             Console.WriteLine("");
             
