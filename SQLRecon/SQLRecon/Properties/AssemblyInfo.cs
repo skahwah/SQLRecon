2b12d49d8bda71e7c308c7a1aed6a8ec8989fd98
Sanjiv Kawa <sanjivkawa@gmail.com>
2023-01-13 09:34:11 -0500

    + another year, another commit

--- SQLRecon/SQLRecon/Properties/AssemblyInfo.cs
+++ SQLRecon/SQLRecon/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SQLRecon")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
