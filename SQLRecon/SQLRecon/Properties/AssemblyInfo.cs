e41d6535d0b6e8885d96b14915dfb4ea293393b7
Sanjiv Kawa <sanjivkawa@gmail.com>
2026-04-02 13:54:28 -0400

    Update AssemblyInfo with new version
    Updated copyright year and assembly version.

--- SQLRecon/SQLRecon/Properties/AssemblyInfo.cs
+++ SQLRecon/SQLRecon/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SQLRecon")]
-[assembly: AssemblyCopyright("Copyright © 2025")]
+[assembly: AssemblyCopyright("Copyright © 2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.10.0")]
-[assembly: AssemblyFileVersion("3.10.0")]
+[assembly: AssemblyVersion("4.0.0")]
+[assembly: AssemblyFileVersion("4.0.0")]
